70d652d7e89b49ad2e739f792a767fa498fe3bdb
Rohit Sarkar <rohitrodg@gmail.com>
2021-07-19 22:53:06 +0530

    jenkinsfile modified

--- vars/pyscript.groovy
+++ vars/pyscript.groovy
@@ -1,4 +1,5 @@
 def call(){
 	def py = libraryResource('func.py')
-	bat 'C:/Users/LENOVO/AppData/Local/Programs/Python/Python39/python.exe ${py}'
+	writeFile file: "pyscript.py", text: py
+	bat 'C:/Users/LENOVO/AppData/Local/Programs/Python/Python39/python.exe pyscript.py'
 }
